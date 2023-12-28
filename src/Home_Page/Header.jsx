ff0bb03f400260f8b5c44b7eb9086ea6c367a86e
Your Name <you@example.com>
2023-12-28 17:08:52 +0530

    new commit

--- src/Home_Page/Header.jsx
+++ src/Home_Page/Header.jsx
@@ -11,13 +11,13 @@ function Header() {
         <nav class="pl-[1.2rem] space-x-4">
           <a
             href="/about"
-            class="bg-[#0F0F0F] text-lg text-[#fff] px-[20px] py-[10px]  rounded-[999px] font-[-apple-system] cursor-pointer [transition:background-color_0.3s,_color_0.3s] hover:rounded-[999px] hover:bg-[#fff] hover:text-[#000] hover:rounded-[45%]"
+            class="bg-black text-lg text-[#fff] px-[20px] py-[10px]  rounded-[999px] font-[-apple-system] cursor-pointer [transition:background-color_0.3s,_color_0.3s] hover:rounded-[999px] hover:bg-[#fff] hover:text-[#000] hover:rounded-[45%]"
           >
             About
           </a>
           <a
             href="/project"
-            class="bg-[#0F0F0F] text-lg text-[#fff] px-[20px] py-[10px]  rounded-[999px] font-[-apple-system] cursor-pointer [transition:background-color_0.3s,_color_0.3s] hover:rounded-[999px] hover:bg-[#fff] hover:text-[#000] hover:rounded-[45%]"
+            class="bg-black text-lg text-[#fff] px-[20px] py-[10px]  rounded-[999px] font-[-apple-system] cursor-pointer [transition:background-color_0.3s,_color_0.3s] hover:rounded-[999px] hover:bg-[#fff] hover:text-[#000] hover:rounded-[45%]"
           >
             Projects
           </a>
@@ -25,7 +25,7 @@ function Header() {
 
         <a
           href="/contact"
-          class="bg-[#0F0F0F] border-[1px] font-[-apple-system] rounded-[999px] border-[solid] text-lg text-[#fff] px-[20px] py-[10px] cursor-pointer [transition:background-color_0.3s,_color_0.3s] hover:bg-[#fff] hover:rounded-[999px] hover:text-[#000] hover:rounded-[45%]"
+          class="bg-black border-[1px] font-[-apple-system] rounded-[999px] border-[solid] text-lg text-[#fff] px-[20px] py-[10px] cursor-pointer [transition:background-color_0.3s,_color_0.3s] hover:bg-[#fff] hover:rounded-[999px] hover:text-[#000] hover:rounded-[45%]"
         >
           Contact
         </a>
